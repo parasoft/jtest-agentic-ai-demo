cc69ddde8fdeb4b53a25be0b0b600a7063c4a3e6
Tomasz Malinowski <mali@parasoft.com>
2026-05-04 13:14:19 +0200

    formatting ExampleLogicTest and emulate more commits in jtest-ai-agent.run.sh

--- .github/workflows/jtest-ai-agent-run.sh
+++ .github/workflows/jtest-ai-agent-run.sh
@@ -5,6 +5,14 @@ echo "jtest-autofix-dummy begin..."
 echo `pwd`
 date >> dummy.txt
 git add dummy.txt
-git commit -m "jtest-autofix-dummy: update dummy.txt"
+git commit -m "jtest-autofix-dummy: 1 update of dummy.txt"
+
+date >> dummy.txt
+git add dummy.txt
+git commit -m "jtest-autofix-dummy: 2 update of dummy.txt"
+
+date >> dummy.txt
+git add dummy.txt
+git commit -m "jtest-autofix-dummy: 3 update of dummy.txt"
 
 echo "jtest-autofix-dummy finished."
