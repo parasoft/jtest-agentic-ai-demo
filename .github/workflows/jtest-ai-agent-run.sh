b78836266b1802a15d77efed5782f00e3dc55314
Tomasz Malinowski <mali@parasoft.com>
2026-05-04 15:37:47 +0200

    added codex cmdline

--- .github/workflows/jtest-ai-agent-run.sh
+++ .github/workflows/jtest-ai-agent-run.sh
@@ -6,7 +6,8 @@ echo "jtest-autofix-dummy begin..."
 # TODO - move to env
 export JTEST_SKILLS_CONFIG=/home/mali/dev/git/github/jtest-agentic-ai-demo/jtest-skills.config
 
-copilot -p "use jtest-static-analysis to analyse and fix violations in the project. Commit each fix separately"  --allow-all-tools  --add-dir /home/mali/dev/git/github/jtest-agentic-ai-demo
+echo "Use jtest-static-analysis to analyse and fix violations in the project. Commit each fix separately" | codex exec --yolo
+#copilot -p "Use jtest-static-analysis to analyse and fix violations in the project. Commit each fix separately"  --allow-all-tools  --add-dir /home/mali/dev/git/github/jtest-agentic-ai-demo
 
 #echo `pwd`
 #date >> dummy.txt
