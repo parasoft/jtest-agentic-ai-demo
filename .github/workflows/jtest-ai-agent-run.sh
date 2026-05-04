1dbb5f8f8599b168e8496e96e1c47d8ba4ad7129
Tomasz Malinowski <mali@parasoft.com>
2026-05-04 15:06:41 +0200

    export settings file

--- .github/workflows/jtest-ai-agent-run.sh
+++ .github/workflows/jtest-ai-agent-run.sh
@@ -2,6 +2,10 @@
 
 echo "jtest-autofix-dummy begin..."
 
+
+# TODO - move to env
+export JTEST_SKILLS_CONFIG=/home/mali/dev/git/github/jtest-agentic-ai-demo/jtest-skills.config
+
 copilot -p "use jtest-static-analysis to analyse and fix violations in the project. Commit each fix separately" --allow-all-tools
 
 #echo `pwd`
