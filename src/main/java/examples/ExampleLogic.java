f9f678a6f4986e3a367f3dccf1624b5df529c89c
Tomasz Malinowski <mali@parasoft.com>
2026-05-04 15:42:51 +0200

    Make ExampleLogic non-cloneable

--- src/main/java/examples/ExampleLogic.java
+++ src/main/java/examples/ExampleLogic.java
@@ -34,4 +34,9 @@ public class ExampleLogic
             _lock.unlock();
         }
     }
+
+    public final Object clone() throws CloneNotSupportedException
+    {
+        throw new CloneNotSupportedException();
+    }
 }
