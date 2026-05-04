3bdd16d79d675c34184c55b87e79f03f737df830
Tomasz Malinowski <mali@parasoft.com>
2026-05-04 15:48:55 +0200

    added unit test creation

--- .github/workflows/jtest-ai-agent-run.sh
+++ .github/workflows/jtest-ai-agent-run.sh
@@ -6,7 +6,8 @@ echo "jtest-autofix-dummy begin..."
 # TODO - move to env
 export JTEST_SKILLS_CONFIG=/home/mali/dev/git/github/jtest-agentic-ai-demo/jtest-skills.config
 
-echo "Use jtest-static-analysis to analyse and fix violations in the project. Commit each fix separately" | codex exec --yolo
+#echo "Use jtest-static-analysis to analyse and fix violations in the project. Commit each fix separately" | codex exec --yolo
+echo "Use jtest-static-analysis to analyse and fix violations in the project. Use jtest-unit-testing to increase coverage. Commit each change separately." | codex exec --yolo
 #copilot -p "Use jtest-static-analysis to analyse and fix violations in the project. Commit each fix separately"  --allow-all-tools  --add-dir /home/mali/dev/git/github/jtest-agentic-ai-demo
 
 #echo `pwd`
