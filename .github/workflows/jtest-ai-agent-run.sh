2459798ea05e5822f3ff02b9655d11e8942e43b9
Tomasz Malinowski <mali@parasoft.com>
2026-05-07 10:47:59 +0200

    f 27 push

--- .github/workflows/jtest-ai-agent-run.sh
+++ .github/workflows/jtest-ai-agent-run.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-echo "jtest-ai-agent-run begin..."
-
-# TODO - move to env
 export JTEST_SKILLS_CONFIG=/home/mali/dev/git/github/jtest-agentic-ai-demo/jtest-skills.config
-
-echo "Use jtest-static-analysis to analyse and fix violations in the project. Commit each fix separately" | codex exec --yolo
-#echo "Use jtest-static-analysis to analyse and fix violations in the project. Use jtest-unit-testing to increase coverage. Commit each change separately. Create a summary and enlist in point what was changed in the project." | codex exec --yolo
-
-#copilot -p "Use jtest-static-analysis to analyse and fix violations in the project. Commit each fix separately"  --allow-all-tools  --add-dir /home/mali/dev/git/github/jtest-agentic-ai-demo
-
-echo "jtest-ai-agent-run finished."
+copilot -p "Use jtest-static-analysis to analyse and fix violations in the project. Use jtest-unit-testing to increase coverage. Commit each change separately. Create a summary and enlist in point what was changed in the project."  --allow-all-tools  --add-dir /home/mali/dev/git/github/jtest-agentic-ai-demo
