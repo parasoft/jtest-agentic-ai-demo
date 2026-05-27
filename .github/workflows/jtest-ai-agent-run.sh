7c26a95f1670e3e4bb7f8a68fa32cbcf169733b6
Brian McGlauflin <bmcglau@parasoft.com>
2026-05-27 11:23:22 -0700

    Remove refs to machine-specific runner paths

--- .github/workflows/jtest-ai-agent-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export JTEST_SKILLS_CONFIG=/home/mali/dev/git/github/jtest-agentic-ai-demo/jtest-skills.config
-copilot -p "Use jtest-static-analysis to analyse and fix violations in the project. Use jtest-unit-testing to increase coverage. Commit each change separately. Create a summary and enlist in point what was changed in the project."  --allow-all-tools  --add-dir /home/mali/dev/git/github/jtest-agentic-ai-demo
